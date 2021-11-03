63e8db7f294f356b4423d38ae0f9a77c68542eaa
eschwarzrock <eschwarzrock@agmednet.com>
2021-11-03 17:16:18 -0400

    Started on the Algorithm entity. based it off of aaron's haven't tested bc the db is down and I don't want to charge us more than needed

--- AlgorithmAppBackend/src/main/java/entities/Algorithm.java
+++ AlgorithmAppBackend/src/main/java/entities/Algorithm.java
@@ -3,13 +3,59 @@ package java.entities;
 public class Algorithm {
 
     private String algoName;
+    private Classification parentClassification;
+    private ArrayList<Implementation> implementations;
 
-    public Algorithm() {
-
+    public Algorithm(String algoName) {
+        this.algoName = algoName;
+        parentClassification = null;
+        implementations = new ArrayList<>();
     }
 
+    public Algorithm(String algoName, Classification parentClassification) {
+        this.algoName = algoName;
+        this.parentClassification = parentClassification;
+        implementations = new ArrayList<>();
+    }
+
+    public Algorithm(String algoName, Classification parentClassification, ArrayList<Implementation> implementations) {
+        this.algoName = algoName;
+        this.parentClassification = parentClassification;
+        this.implementations = implementations;
+    }
+
+    //getters and setters
     public String getAlgoName() {
         return algoName;
     }
 
+    public Classification getParentClassification() {
+        return parentClassification;
+    }
+
+    public ArrayList<Implementation> getImplementations() {
+        return implementations;
+    }
+
+    public void setAlgoName(String algoName) {
+        this.algoName = algoName;
+    }
+
+    public void setImplementations(ArrayList<Implementation> implementations) {
+        this.implementations = implementations;
+    }
+
+    public void setParentClassification(Classification parentClassification) {
+        this.parentClassification = parentClassification;
+    }
+
+    //other
+
+    public boolean addImplementation(Implementation i) {
+        return implementations.add(i);
+    }
+
+    public boolean removeImplementation(String implName) {
+        return implementations.removeIf(a -> a.getImplName().equals(implName));
+    }
 }
---
+++ AlgorithmAppBackend/src/main/java/entities/Implementation.java
@@ -0,0 +1,11 @@
+package java.entities;
+
+public class Implementation {
+
+    private String implName;
+
+    public String getImplName() {
+        return implName;
+    }
+
+}
