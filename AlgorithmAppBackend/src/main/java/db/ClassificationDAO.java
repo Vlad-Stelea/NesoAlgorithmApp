1c5c0ff1749a9fc812630ff4cc27b768f2f4e724
adkrueger <adkrueger@wpi.edu>
2021-11-03 22:02:23 -0400

    fixing package and imports of similar packages

--- AlgorithmAppBackend/src/main/java/db/ClassificationDAO.java
+++ AlgorithmAppBackend/src/main/java/db/ClassificationDAO.java
@@ -1,6 +1,6 @@
-package java.db;
+package db;
 
-import java.entities.Classification;
+import entities.Classification;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
