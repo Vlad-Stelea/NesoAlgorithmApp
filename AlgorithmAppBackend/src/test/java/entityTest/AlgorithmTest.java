1a2957ecf10d67da614eece9c7b599296bac18dd
eschwarzrock <eschwarzrock@agmednet.com>
2021-11-05 15:56:37 -0400

    Added test cases

--- AlgorithmAppBackend/src/test/java/entityTest/AlgorithmTest.java
+++ AlgorithmAppBackend/src/test/java/entityTest/AlgorithmTest.java
@@ -1,4 +1,46 @@
 package entityTest;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import entities.Algorithm;
+import entities.Classification;
+import entities.Implementation;
+import helloworld.App;
+import org.junit.Test;
+
+import java.util.ArrayList;
 
 public class AlgorithmTest {
+    @Test
+    public void testAddImpl() {
+        Algorithm a = new Algorithm("test");
+        ArrayList<Implementation> empty = new ArrayList<>();
+        assertEquals(a.getAlgoName(), "test");
+        assertEquals(a.getParentClassification(), null);
+        assertEquals(a.getImplementations(), empty);
+        Implementation I = new Implementation("testImpl");
+        a.addImplementation(I);
+        assertEquals(a.getImplementations().get(0), I);
+
+    }
+
+    @Test
+    public void testRemoveImpl() {
+        Algorithm a = new Algorithm("test");
+        ArrayList<Implementation> empty = new ArrayList<>();
+        Implementation I = new Implementation("testImpl");
+        a.addImplementation(I);
+        Classification parent = new Classification("parent");
+        a.setAlgoName("test2");
+        a.setParentClassification(parent);
+        a.removeImplementation("testImpl");
+
+        assertEquals(a.getAlgoName(), "test2");
+        assertEquals(a.getParentClassification(), parent);
+        assertEquals(a.getImplementations(), empty);
+
+    }
+
 }
