fd0ce2ec34b7b285cb9ca0b5baa2d8c41600fa4d
Vlad-Stelea <vladstelea@gmail.com>
2021-11-08 09:48:44 -0500

    Made frontend deployable

--- frontend/js/config/liveConfig.js
+++ frontend/js/config/liveConfig.js
@@ -1,5 +1,4 @@
 // config file used for deployments
 const LiveConfig = {
-    // TODO change to the real value
-    LOGIN_URL_CALLBACK : "https://nesoalgorithmapp.s3.us-east-2.amazonaws.com/frontend/html/TestRegisterUserCallback.html"
+    LOGIN_URL_CALLBACK : "https://nesoalgorithmapp.s3.us-east-2.amazonaws.com/frontend/html/NavContainer.html"
 }
