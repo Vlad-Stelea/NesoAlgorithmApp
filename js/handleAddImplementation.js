759575d0514187143526d2ed11c6920f6985c45f
Tjcampbell <your708@gmail.com>
2021-11-09 00:17:45 -0500

    Add the functionality to create a new implementation just need to connect it to the lambda function

---
+++ js/handleAddImplementation.js
@@ -0,0 +1,24 @@
+function handleAddImpPrep(){
+    console.log("addImpPrep")
+    var addTopClassForm = document.getElementById('AddImpForm');
+   // Update Hierarchy result
+   addTopClassForm.innerHTML = ' <br><label for="ImplementationName">Implementation name:</label>' +
+                            '<input type="text" id="fname" name="fname">'+
+                            '<label for="Code">Code:</label>' +
+                                                        '<input type="text" id="Code" name="Code">'+
+                            '<label for="Language">Language:</label>' +
+                                    '<input type="text" id="lang" name="lang">'+
+                            '<input type="submit" value="Submit" onclick="handleAddTopLevelSubmit(this)"><br><br>'
+
+}
+
+
+function handleAddTopLevelSubmit(ele){
+    console.log("addTopSubmit")
+    console.log(ele.parentElement.children[2].value)
+    console.log(ele.parentElement.children[4].value)
+    console.log(ele.parentElement.children[6].value)
+    var addTopClassForm = document.getElementById('AddTopForm');
+   // Update Hierarchy result
+   addTopClassForm.innerHTML = ''
+}
