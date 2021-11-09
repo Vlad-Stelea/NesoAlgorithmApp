f384e86684f67d0cce5e7a9ae6c09aa8bb16d807
Tjcampbell <your708@gmail.com>
2021-11-09 00:35:57 -0500

    Created a similar function to adding a new classification in Implementations still have multiple todos

--- js/handleAddImplementation.js
+++ js/handleAddImplementation.js
@@ -1,6 +1,6 @@
 function handleAddImpPrep(){
     console.log("addImpPrep")
-    var addTopClassForm = document.getElementById('AddImpForm');
+    let addTopClassForm = document.getElementById('AddImpForm');
    // Update Hierarchy result
    addTopClassForm.innerHTML = ' <br><label for="ImplementationName">Implementation name:</label>' +
                             '<input type="text" id="fname" name="fname">'+
@@ -18,7 +18,52 @@ function handleAddTopLevelSubmit(ele){
     console.log(ele.parentElement.children[2].value)
     console.log(ele.parentElement.children[4].value)
     console.log(ele.parentElement.children[6].value)
-    var addTopClassForm = document.getElementById('AddTopForm');
+    let addTopClassForm = document.getElementById('AddTopForm');
    // Update Hierarchy result
-   addTopClassForm.innerHTML = ''
+   let iData = {}
+   let implementationName = ele.parentElement.children[2].value
+   let code = ele.parentElement.children[4].value
+   let Language = ele.parentElement.children[6].value
+   iData["implName"] = implementationName
+   iData["codeUrl"] = code
+   iData["language"] = Language
+   //TODO:Add the algorithm name here
+   if(implementationName === "") {
+        alert("Please enter an Implementation name")
+   } else {
+            let js = JSON.stringify(iData)
+            console.log("Create Implementation JSON: " + js);
+            let xhr = new XMLHttpRequest();
+
+            /**xhr.open("POST", createClassification_url, true);
+            xhr.send(js);
+            console.log("sent create classification request");
+
+
+
+            // after we get a response
+            xhr.onloadend = function() {
+                console.log("create classification response: " + xhr);
+
+                if(xhr.readyState === XMLHttpRequest.DONE) {
+                    if(xhr.status === 200) {
+                        console.log("XHR: " + xhr.responseText);
+                        processCreateClassificationResponse(xhr.responseText);
+                    }
+                    else {
+                        console.log("Status != 200. Actual create response: " + xhr.responseText);
+                        let newJS = JSON.parse(xhr.responseText);
+                        let err = newJS["response"];
+                        alert(err);
+                    }
+                }
+                else {
+                    processCreateClassificationResponse("N/A");
+                }
+            };*/
+
+
+   }
+
+
 }
