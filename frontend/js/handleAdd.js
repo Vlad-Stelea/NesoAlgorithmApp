3a42443253c2e5bca70f05aa8a866fc9079556e3
eschwarzrock <eschwarzrock@agmednet.com>
2021-11-09 10:00:51 -0500

    fixed one last bug, ready to push!

--- frontend/js/handleAdd.js
+++ frontend/js/handleAdd.js
@@ -5,17 +5,25 @@ function handleAdd(ele){
     let ender = '</li>'
     let li = ele.parentElement.innerHTML
     let hierarchyHTML = document.getElementById('Hierarchy').innerHTML
-    //front half
-    let newHierarchyHTML = hierarchyHTML.substr(0, hierarchyHTML.indexOf(li) + li.length + ender.length)
-    //add in and append the rest of the html
-    newHierarchyHTML = newHierarchyHTML +
-        '<ul style="list-style: none;">' +
+    let formHTML = '<ul style="list-style: none;">' +
         '<li>'+
         ' <br><label for="Algorithm Name">Algorithm name:</label>' +
         '<input type="text" id="fname" name="fname">'+
         '<input type="submit" value="Submit" onclick="handleAddAlgorithmSubmit(\''+ ele.parentElement.children[1].textContent +'\', this)"><br><br>' +
         '</li>' +
-        '</ul>' + hierarchyHTML.substr(hierarchyHTML.indexOf(li) + li.length + ender.length)
+        '</ul>'
+    let newHierarchyHTML = ""
+    //check if add was allready clicked
+    if(hierarchyHTML.indexOf(formHTML) == hierarchyHTML.indexOf(li) + li.length + ender.length){
+        //remove the form
+        newHierarchyHTML = hierarchyHTML.substr(0, hierarchyHTML.indexOf(formHTML)) + hierarchyHTML.substr( hierarchyHTML.indexOf(formHTML)+formHTML.length)
+    }else {
+        //add the form
+        //front half
+        newHierarchyHTML = hierarchyHTML.substr(0, hierarchyHTML.indexOf(li) + li.length + ender.length)
+        //add in and append the rest of the html
+        newHierarchyHTML = newHierarchyHTML + formHTML + hierarchyHTML.substr(hierarchyHTML.indexOf(li) + li.length + ender.length)
+    }
     document.getElementById('Hierarchy').innerHTML = newHierarchyHTML
 }
 
