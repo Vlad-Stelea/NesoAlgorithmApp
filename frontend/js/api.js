00e1df79ff3205332aa9fc627742a18af16f176f
adkrueger <adkrueger@wpi.edu>
2021-11-08 22:02:11 -0500

    updating HTML/JS to current working state (need to integrate display)

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,5 +1,5 @@
 // URL for deployed API in API Gateway
-let api_url = "https://jdkme371z4.execute-api.us-east-2.amazonaws.com/preAlpha";
+let api_url = "https://jdkme371z4.execute-api.us-east-2.amazonaws.com/preAlpha/";
 
 let createClassification_url = api_url + "Classification";
 let getHierarchy_url = api_url + "Classification/Hierarchy";
