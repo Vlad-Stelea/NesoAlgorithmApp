71a139d1920d8a88f7c8f9c3d83a0f8358c775f1
Tjcampbell <your708@gmail.com>
2021-11-21 12:15:46 -0500

    Intial changes made need to merge with master

--- AlgorithmAppBackend/src/main/java/CreateBenchmark/CreateBenchmarkResponse.java
+++ AlgorithmAppBackend/src/main/java/CreateBenchmark/CreateBenchmarkResponse.java
@@ -3,29 +3,113 @@ package CreateBenchmark;
 
 import com.google.gson.Gson;
 
+import java.sql.Date;
+import static Utils.EqualityUtils.equalOrBothNull;
+
 public class CreateBenchmarkResponse {
-    public final String response;
+
     public final int httpCode;
     public final String error;
+    String benchID;
+    String benchName;
+    String algoName;
+    String machineConfigName;
+    String implName;
+    String problemInstanceName;
+    Date dateRun;
 
-
-    public CreateBenchmarkResponse (String s, int code) {
-        this.response = s;
+    public CreateBenchmarkResponse (int code, String benchID, String benchName, String algoName, String machineConfigName, String implName, String problemInstanceName, Date dateRun) {
+        this.benchID = benchID;
+        this.benchName = benchName;
+        this.algoName = algoName;
+        this.machineConfigName = machineConfigName;
+        this.implName = implName;
+        this.problemInstanceName = problemInstanceName;
+        this.dateRun = dateRun;
         this.httpCode = code;
         this.error = "";
     }
 
     // 200 means success
-    public CreateBenchmarkResponse (String s, int code, String error) {
-        this.response = s;
+    public CreateBenchmarkResponse (int code, String error) {
         this.httpCode = code;
         this.error = error;
+    }
+
+    public String getBenchID() {
+        return benchID;
+    }
+
+    public void setBenchID(String benchID) {
+        this.benchID = benchID;
+    }
+
+    public String getBenchName() {
+        return benchName;
+    }
+
+    public void setBenchName(String benchName) {
+        this.benchName = benchName;
+    }
+
+    public String getAlgoName() {
+        return algoName;
+    }
+
+    public void setAlgoName(String algoName) {
+        this.algoName = algoName;
+    }
+
+    public String getMachineConfigName() {
+        return machineConfigName;
+    }
+
+    public void setMachineConfigName(String machineConfigName) {
+        this.machineConfigName = machineConfigName;
+    }
+
+    public String getImplName() {
+        return implName;
+    }
+
+    public void setImplName(String implName) {
+        this.implName = implName;
+    }
+
+    public String getProblemInstanceName() {
+        return problemInstanceName;
+    }
+
+    public void setProblemInstanceName(String problemInstanceName) {
+        this.problemInstanceName = problemInstanceName;
+    }
+
+    public Date getDateRun() {
+        return dateRun;
+    }
+
+    public void setDateRun(Date dateRun) {
+        this.dateRun = dateRun;
     }
 
     public String toString() {
         Gson gson = new Gson();
         return gson.toJson(this);
     }
-
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) return false;
+        if(o instanceof CreateBenchmarkResponse) {
+            CreateBenchmarkResponse otherResponse = (CreateBenchmarkResponse) o;
+            return otherResponse.statusCode == this.statusCode &&
+                    equalOrBothNull(otherResponse.implName, this.implName) &&
+                    equalOrBothNull(otherResponse.algoName, this.algoName) &&
+                    equalOrBothNull(otherResponse.codeUrl, this.codeUrl) &&
+                    equalOrBothNull(otherResponse.language, this.language) &&
+                    equalOrBothNull(otherResponse.error, this.error);
+        } else {
+            return false;
+        }
+    }
 
 }
