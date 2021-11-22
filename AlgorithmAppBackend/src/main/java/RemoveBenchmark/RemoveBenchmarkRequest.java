67aebf1ca66ffdc52f420d2fd7096ba7f04a2fc2
adkrueger <adkrueger@wpi.edu>
2021-11-22 16:36:40 -0500

    Lambda functionality for removing benchmarks

---
+++ AlgorithmAppBackend/src/main/java/RemoveBenchmark/RemoveBenchmarkRequest.java
@@ -0,0 +1,27 @@
+package RemoveBenchmark;
+
+import com.google.gson.Gson;
+
+public class RemoveBenchmarkRequest {
+
+    String benchmarkID;
+
+    public RemoveBenchmarkRequest(String benchmarkID) {
+        this.benchmarkID = benchmarkID;
+    }
+
+    public String getBenchmarkID() {
+        return benchmarkID;
+    }
+
+    public void setBenchmarkID(String benchmarkID) {
+        this.benchmarkID = benchmarkID;
+    }
+
+    @Override
+    public String toString() {
+        Gson gson = new Gson();
+        return gson.toJson(this);
+    }
+
+}
