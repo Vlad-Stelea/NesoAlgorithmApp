532fc410c660b640a55b96f9915922d45a7fd4a2
Vlad-Stelea <vladstelea@gmail.com>
2021-12-06 17:14:10 -0500

    Parse token for username

--- frontend/js/viewModel/user.js
+++ frontend/js/viewModel/user.js
@@ -2,4 +2,17 @@ class User {
     constructor() {
         this.token = null;
     }
+
+    get username() {
+        // Make sure token is set already
+        if(this.token === null) return null;
+        // parse the token out
+        return this.parseUsername(this.token);
+    }
+
+    parseUsername(token) {
+        let encodedPayload = token.split('.')[1];
+        return JSON.parse(atob(encodedPayload))["cognito:username"];
+    }
+
 }
