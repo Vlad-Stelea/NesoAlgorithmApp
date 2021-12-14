a540da7ef7a1e5b9e43a50661e993b0c2a69ee0c
Vlad-Stelea <vladstelea@gmail.com>
2021-12-14 13:43:03 -0500

    Problem Instances

--- AlgorithmAppBackend/src/test/java/entities/ProblemInstanceTest.java
+++ AlgorithmAppBackend/src/test/java/entities/ProblemInstanceTest.java
@@ -23,4 +23,25 @@ public class ProblemInstanceTest {
         assertNotEquals(problemInstance, new ProblemInstance("NOT_probInstanceEntityTest", "probInstanceEntityTest_algo"));
     }
 
+    @Test
+    public void testGetsAndSets() {
+        String uuid = "uuid";
+        String algoName = "algoName";
+        String datasetUrl = "datasetUrl";
+        String probInstancename = "probInstanceName";
+
+        ProblemInstance pInstance = new ProblemInstance(uuid, algoName);
+
+        pInstance.setDatasetURL(datasetUrl);
+        pInstance.setAlgoName(algoName);
+        pInstance.setProbInstanceUUID(uuid);
+        pInstance.setProbInstanceName(probInstancename);
+
+        assertEquals(uuid, pInstance.getProbInstanceUUID());
+        assertEquals(algoName, pInstance.getAlgoName());
+        assertEquals(datasetUrl, pInstance.getDatasetURL());
+        assertEquals(probInstancename, pInstance.getProbInstanceName());
+
+    }
+
 }
