a6889deec4c923e2e6dbc845ccd96e25abe95845
efschwarzrock <efschwarzrock@wpi.edu>
2021-12-13 23:40:33 -0500

    more

--- AlgorithmAppBackend/src/test/java/lambda/CreateImplementationTest.java
+++ AlgorithmAppBackend/src/test/java/lambda/CreateImplementationTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 import java.sql.SQLException;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -54,6 +55,22 @@ public class CreateImplementationTest extends LambdaTest {
     }
 
     @Test
+    public void testAlreadyHaveImplementation() throws SQLException {
+        // Mock the dao and storage classes
+        when(dao.hasImplementation("childTest", "TestClass")).thenReturn(true);
+        when(dao.createImplementation(any(), any(), any(), any())).thenReturn(true);
+        when(storage.storeImplementation(any(), any())).thenReturn("www.fakeurl.com");
+
+        CreateImplementationResponse handleResponse = caHandler.handle(reqWithParent);
+
+        CreateImplementationResponse expectedResponse = new CreateImplementationResponse(
+                409,
+                "Implementation already exists"
+        );
+        assertEquals(expectedResponse, handleResponse);
+    }
+
+    @Test
     public void testFailCreateImplementation() throws SQLException {
         // add the implementation with a null parent, mock that the parent was added already, and ensure the handler responds appropriately
         when(dao.createImplementation("childTest", "TestCode","TestLang", "TestClass")).thenReturn(false);
@@ -64,6 +81,56 @@ public class CreateImplementationTest extends LambdaTest {
                 "Implementation already exists"
         );
         assertEquals(expectedResponse, handleResponse);
+    }
+
+    @Test
+    public void testNoDBConnection() throws SQLException {
+        // add the implementation with a null parent, mock that the parent was added already, and ensure the handler responds appropriately
+        when(dao.hasImplementation("childTest", "TestClass")).thenThrow(new NullPointerException());
+        CreateImplementationResponse handleResponse = caHandler.handle(reqWithParent);
+
+        CreateImplementationResponse expectedResponse = new CreateImplementationResponse(
+                400,
+                "Unknown Error"
+        );
+        assertEquals(expectedResponse, handleResponse);
+    }
+
+    @Test
+    public void testRequestAndResponceClasses() throws SQLException {
+        // add the implementation with a null parent, mock that the parent was added already, and ensure the handler responds appropriately
+        CreateImplementationRequest req = new CreateImplementationRequest();
+        req.setAlgoName("test");
+        assertEquals(req.getAlgoName(),"test");
+        req.setCode("test1");
+        assertEquals(req.getCode(),"test1");
+        req.setImplName("test2");
+        assertEquals(req.getImplName(),"test2");
+        req.setLanguage("test3");
+        assertEquals(req.getLanguage(),"test3");
+        req.setFileExtension("test4");
+        assertEquals(req.getFileExtension(),"test4");
+
+        assertEquals(req.toString(), "{\"implName\":\"test2\",\"algoName\":\"test\",\"code\":\"test1\",\"fileExtension\":\"test4\",\"language\":\"test3\"}");
+
+        CreateImplementationResponse response = new CreateImplementationResponse(20, "d");
+        assertFalse(response.equals(dao));
+        response.setAlgoName("test");
+        assertEquals(response.getAlgoName(), "test");
+        response.setCodeUrl("test1");
+        assertEquals(response.getCodeUrl(), "test1");
+        response.setError("test2");
+        assertEquals(response.getError(), "test2");
+        response.setImplName("test3");
+        assertEquals(response.getImplName(), "test3");
+        response.setLanguage("test4");
+        assertEquals(response.getLanguage(), "test4");
+        response.setStatusCode(123);
+        assertEquals(response.getStatusCode(), 123);
+
+        assertEquals(response.toString(), "{\"statusCode\":123,\"implName\":\"test3\",\"algoName\":\"test\",\"codeUrl\":\"test1\",\"language\":\"test4\",\"error\":\"test2\"}");
+
+
     }
 
 
