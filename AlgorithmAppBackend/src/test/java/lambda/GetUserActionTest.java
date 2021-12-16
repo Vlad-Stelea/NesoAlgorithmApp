54f5ce7f20b9a0e842b1200017244f559858c0ad
adkrueger <adkrueger@wpi.edu>
2021-12-16 12:34:07 -0500

    get user actions coverage

--- AlgorithmAppBackend/src/test/java/lambda/GetUserActionTest.java
+++ AlgorithmAppBackend/src/test/java/lambda/GetUserActionTest.java
@@ -1,12 +1,11 @@
 package lambda;
 
+import GetUserActions.GetUserActions;
 import GetUserActions.GetUserActionsHandler;
 import GetUserActions.GetUserActionsRequest;
 import GetUserActions.GetUserActionsResponse;
-import ReclassifyAlgorithm.ReclassifyAlgorithmHandler;
-import ReclassifyAlgorithm.ReclassifyAlgorithmRequest;
+import com.amazonaws.services.lambda.runtime.Context;
 import db.ActivityLogDAO;
-import db.AlgorithmDAO;
 import entities.UserAction;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,7 +14,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -44,11 +43,48 @@ public class GetUserActionTest {
      }
 
      @Test
+     public void testRequestClass() {
+          GetUserActionsRequest req = new GetUserActionsRequest();
+          req.setUsername("helloWorld");
+
+          GetUserActionsRequest anotherReq = new GetUserActionsRequest("helloWorld");
+          assertEquals(req.getUsername(), anotherReq.getUsername());
+          assertEquals(req.toString(), anotherReq.toString());
+     }
+
+     @Test
      public void GetAllUserActionsTest() throws SQLException {
           when(dao.getUserAction("jack")).thenReturn(action);
 
           GetUserActionsResponse response = rcHandler.handle(rcRequest);
           assertEquals(response.getUserActionList(), action);
+     }
 
+     @Test
+     public void testMainGetAllUserActions() throws SQLException {
+          // mock the main lambda function
+          GetUserActionsHandler mockHandler = mock(GetUserActionsHandler.class);
+          GetUserActions mainLambda = new GetUserActions(mockHandler);
+          GetUserActionsRequest mockRequest = new GetUserActionsRequest("some_username");
+          Context mockContext = new TestContext();
+
+          GetUserActionsResponse mockResponse = new GetUserActionsResponse(new ArrayList<>(), "some_username", 200);
+          when(mainLambda.handleRequest(mockRequest, mockContext)).thenReturn(mockResponse);
+          when(dao.getUserAction("some_username")).thenReturn(new ArrayList<>());
+          GetUserActionsResponse receivedResponse = mainLambda.handleRequest(mockRequest, mockContext);
+
+          assertEquals(mockResponse, receivedResponse);
+          assertEquals(mockResponse.toString(), receivedResponse.toString());
+     }
+
+     @Test
+     public void testFailGetAllUserActions() throws SQLException {
+          when(dao.getUserAction("jack")).thenThrow(NullPointerException.class);
+
+          GetUserActionsResponse expectedResponse = new GetUserActionsResponse(400, "Unable to get userActions\n");
+          GetUserActionsResponse actualResponse = rcHandler.handle(rcRequest);
+
+          assertTrue(actualResponse.getError().contains(expectedResponse.getError()));
+          assertEquals(expectedResponse.getStatusCode(), actualResponse.getStatusCode());
      }
 }
